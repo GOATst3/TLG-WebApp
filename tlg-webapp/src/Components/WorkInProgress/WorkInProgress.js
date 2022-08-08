eae590d013353bbd7771e07363d255a5aec9c7a5
Goat.St3 <stefano2301@live.it>
2022-08-08 20:35:30 +0200

    workinprogress page add

---
+++ tlg-webapp/src/Components/WorkInProgress/WorkInProgress.js
@@ -0,0 +1,12 @@
+import React from "react"
+const WorkInProgress= ()=>
+
+  <>
+   <h1 style={{height:'60vh',textAlign:'center', paddingTop:'100px'}}>WorkInProgress </h1>
+
+
+   
+   </>
+
+
+export default WorkInProgress
