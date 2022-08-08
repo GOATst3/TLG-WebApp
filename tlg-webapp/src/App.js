0782469b924e6a992fdcbed5d535306b66c1d02c
Goat.St3 <stefano2301@live.it>
2022-08-08 21:25:11 +0200

    login implemented

--- tlg-webapp/src/App.js
+++ tlg-webapp/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import WorkInProgress from './Components/WorkInProgress/WorkInProgress';
+import Landing from './Components/Landing/Landing';
+import Dashboard from './Components/Dashboard/Dashboad'
 
 function App() {
+
+  const logged=localStorage.getItem('logged')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={logged?<Navigate replace to='Dashboard'/> : <Landing/>}/>
+          <Route path="WorkInProgress" element={<WorkInProgress />} />
+          <Route path='Dashboard' element={!logged? <Navigate replace to='/'/> : <Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
